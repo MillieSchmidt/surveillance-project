8a80f926ead7f6c5f9ce0a8e7c7eab81995d2691
Millie Schmidt <millenium2124@gmail.com>
2024-10-03 12:01:22 +0300

    updated links to my bookstore instead of to personal website

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -7,7 +7,7 @@ const MobileNav = () => {
         <div className='mobile-navigation'>
             <Link className='nav-item' to='/posts'>מאמרים</Link>|
             <Link className='nav-item' to='/about'>אודות הפרויקט</Link>|
-            <a className='nav-item' href='https://millieschmidt.net/' rel='noreferrer' target='_blank'>חנות</a>
+            <a className='nav-item' href='https://millieschmidt.github.io/bookstore/' rel='noreferrer' target='_blank'>חנות</a>
         </div>
     );
 }
